d89126a479a87084de3605ec94861d1e8b7135c0
campellcl <campellcl@email.appstate.edu>
2015-10-19 11:33:34 -0400

    Updated Skeleton Code

--- assign1/separate.pl
+++ assign1/separate.pl
@@ -1,5 +1,11 @@
 /* sperate.pl */
 /* Takes a list as input paremeter and two lists as output parameters.
     The incoming list is sepreated into the two output lists. */
-/*base case for separate empty list: */
-separate([],[] | []).
+
+/* base case for separate empty list: */
+separate([],[]| []).
+/* base case for one element list: */
+
+/* production for recursive function separate: */
+
+
